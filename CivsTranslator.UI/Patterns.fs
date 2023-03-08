6be2dd6a8c8f5b26ea2170df41263a0b24606499
Dineinger <45884614+Dineinger@users.noreply.github.com>
2023-03-08 13:58:05 +0100

    pattern improved and corrected, catches exceptions

--- CivsTranslator.UI/Patterns.fs
+++ CivsTranslator.UI/Patterns.fs
@@ -1,5 +1,5 @@
 ﻿module CivsTranslator.Patterns
 open System.Text.RegularExpressions
 
-let listPoint = Regex("\s*\* \"[\w\s\d\.\()ℳ]*\"\s*")
+let listPoint = Regex("[ \t]*\* ((\"[\w\s\d\.\(\)ℳ]*\")|(\{No\})|(\{Yes\}))[ \t]*")
 let listHead = Regex("\s*\"[\w\s\d\.\()ℳ]*\" >\s*")
--- CivsTranslator.UI/Program.fs
+++ CivsTranslator.UI/Program.fs
@@ -6,37 +6,41 @@ try
     Console.OutputEncoding = Text.Encoding.UTF8 |> ignore
 finally
 
-printf "Quelle: "
-let source = Console.ReadLine()
-printf "Ziel: "
-let destination = Console.ReadLine()
+try
 
-let content = File.ReadAllLines(source)
-let mcCode =
-    ItemReader.read(content)
-    |> ItemToText.convert
+    printf "Quelle: "
+    let source = Console.ReadLine()
+    printf "Ziel: "
+    let destination = Console.ReadLine()
 
-printfn "%s" mcCode
+    let content = File.ReadAllLines(source)
+    let mcCode =
+        ItemReader.read(content)
+        |> ItemToText.convert
 
-if File.Exists destination then
-    printfn "Datei existiert bereits: %s" destination
-    printfn "Wollen sie sie ersetzen? (y/n)"
-    let mutable run = true
-    while run do
+    printfn "%s" mcCode
+
+    if File.Exists destination then
         printfn "Datei existiert bereits: %s" destination
         printfn "Wollen sie sie ersetzen? (y/n)"
-        let key = Console.ReadLine()
-        match key with
-        | "y" ->
-            File.WriteAllText(destination, mcCode)
-            printfn "saved at: %s" destination
-            run <- false
-        | "n" ->
-            printfn "exits..."
-            run <- false
-        | _ -> ()
-else
-    File.WriteAllText(destination, mcCode)
-    printfn "saved at: %s" destination
+        let mutable run = true
+        while run do
+            printfn "Datei existiert bereits: %s" destination
+            printfn "Wollen sie sie ersetzen? (y/n)"
+            let key = Console.ReadLine()
+            match key with
+            | "y" ->
+                File.WriteAllText(destination, mcCode)
+                printfn "saved at: %s" destination
+                run <- false
+            | "n" ->
+                printfn "exits..."
+                run <- false
+            | _ -> ()
+    else
+        File.WriteAllText(destination, mcCode)
+        printfn "saved at: %s" destination
 
-printfn "exited"
+    printfn "exited"
+with e ->
+    printfn "%s" e.Message
