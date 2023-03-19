a95a6e25dd74fb2acf7d965c2f35e98333de3182
Dineinger <45884614+Dineinger@users.noreply.github.com>
2023-03-19 17:02:35 +0100

    adding Colors class containing color codes from a json text

---
+++ McTextTransforming.TextGeneration/Colors.fs
@@ -0,0 +1,25 @@
+﻿namespace McTextTransforming.TextGeneration
+open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+type Colors [<JsonConstructor>](white, lightGray, green, red, lightGrayBlue, darkCyan) =
+    [<JsonPropertyName("white")>]
+    member _.White : string = white
+    [<JsonPropertyName("light-gray")>]
+    member _.LightGray : string = lightGray
+    [<JsonPropertyName("green")>]
+    member _.Green : string = green
+    [<JsonPropertyName("red")>]
+    member _.Red : string = red
+    [<JsonPropertyName("light-gray-blue")>]
+    member _.LightGrayBlue : string = lightGrayBlue
+    [<JsonPropertyName("dark-cyan")>]
+    member _.DarkCyan : string = darkCyan
+
+module Colors =
+    let fromJson (json : string) : Result<Colors, Exception> =
+        try
+            Ok(JsonSerializer.Deserialize(json))
+        with e ->
+            Error(e)
