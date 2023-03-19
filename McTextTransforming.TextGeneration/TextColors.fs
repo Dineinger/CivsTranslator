143ff1527fce6acbf653a47fdf2996b9267587a7
Dineinger <45884614+Dineinger@users.noreply.github.com>
2023-03-19 17:10:32 +0100

    adding TextColors class containing color codes for different text.

---
+++ McTextTransforming.TextGeneration/TextColors.fs
@@ -0,0 +1,25 @@
+﻿namespace McTextTransforming.TextGeneration
+open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+type TextColors [<JsonConstructor>](h1, text, listHeader, point, yes, no) =
+    [<JsonPropertyName("h1")>]
+    member _.H1 = h1
+    [<JsonPropertyName("text")>]
+    member _.Text = text
+    [<JsonPropertyName("list-header")>]
+    member _.ListHeader = listHeader
+    [<JsonPropertyName("point")>]
+    member _.Point = point
+    [<JsonPropertyName("yes")>]
+    member _.Yes = yes
+    [<JsonPropertyName("no")>]
+    member _.No = no
+
+module TextColors =
+    let fromJson (json : string) : Result<TextColors, Exception> =
+        try
+            Ok(JsonSerializer.Deserialize(json))
+        with e ->
+            Error(e)
